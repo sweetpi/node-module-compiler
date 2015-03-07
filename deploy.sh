c69528cb17f1c5d246b132e1e1508badc44aed2e
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-03-07 08:53:44 +0100

    moved decryption back to travis file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -e
-openssl aes-256-cbc -K $encrypted_161a5cc809f6_key -iv $encrypted_161a5cc809f6_iv -in deploy_key.enc -out deploy_key -d
 chmod 600 ./deploy_key
 ssh-add ./deploy_key
 git clone git@github.com:pimatic-ci/sqlite3.git repo
