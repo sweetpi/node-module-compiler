d33806f606799d541678f9983ebee01de2b893bf
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-03-07 09:10:22 +0100

    Test

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh -e
+echo $CPP
 VERBOSE=1 npm install sqlite3 --target_arch=arm
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,9 @@ ssh-add ./deploy_key
 git clone git@github.com:pimatic-ci/sqlite3.git repo
 cp -R ./node_modules/sqlite3/ repo/
 cd repo
-git config user.email "oliverschneider89+sweetpi@gmail.com"
+rm -r *
+git rm $(git ls-files --deleted)  
+git config user.email "oliverschneider89+pimatic-ci@gmail.com"
 git config user.name "pimatic-ci"
 git add -A
 git commit -a -m "new build"
