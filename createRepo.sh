1d87af4763873c0ae8e1324b727f4155440c58ec
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-03-07 15:24:06 +0100

    fix for init repo readme

--- createRepo.sh
+++ createRepo.sh
@@ -2,7 +2,7 @@
 curl -u "${GITHUB_TOKEN}:x-oauth-basic" https://api.github.com/user/repos -d "{\"name\":\"${MODULE}\"}"
 mkdir repo
 cd repo
-echo "# Releases of pimatic-mobile-frontend" >> README.md
+echo "# Releases of ${MODULE}" >> README.md
 git init
 git add README.md
 git commit -m "init"
