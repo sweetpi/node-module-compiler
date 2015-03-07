ed7ad2d310f68e8e1cb446a00d7031b2ac8c519c
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-03-07 10:03:47 +0100

    fix for copy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ eval "$(ssh-agent)"
 ssh-add ./deploy_key
 git clone git@github.com:pimatic-ci/sqlite3.git repo
 rm -r -f ./repo/*
-cp -R ./node_modules/sqlite3 ./repo
+cp -R ./node_modules/sqlite3/* ./repo/
 cd repo
 git config user.email "oliverschneider89+pimatic-ci@gmail.com"
 git config user.name "pimatic-ci"
