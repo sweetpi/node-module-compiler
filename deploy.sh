fd43b4866e320c315666adc8d896c71fa8e73197
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-03-07 09:57:43 +0100

    removed git remove

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ git clone git@github.com:pimatic-ci/sqlite3.git repo
 rm -r -f ./repo/*
 cp -R ./node_modules/sqlite3 ./repo
 cd repo
-git rm --ignore-unmatch -- $(git ls-files --deleted)  
 git config user.email "oliverschneider89+pimatic-ci@gmail.com"
 git config user.name "pimatic-ci"
+#git rm --ignore-unmatch -- $(git ls-files --deleted)  
 git add -A
 git commit -a -m "new build"
 git push
