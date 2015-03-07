ddbbabc177b284da6635b799af2a3002d68c322e
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-03-07 09:01:33 +0100

    added git idendity

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ ssh-add ./deploy_key
 git clone git@github.com:pimatic-ci/sqlite3.git repo
 cp -R ./node_modules/sqlite3/ repo/
 cd repo
+git config user.email "oliverschneider89+sweetpi@gmail.com"
+git config user.name "pimatic-ci"
 git add -A
 git commit -a -m "new build"
 git push
