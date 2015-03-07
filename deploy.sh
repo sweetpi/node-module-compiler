f609bbdaf0d9d4dff57d10f497cb402289ccdc81
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2015-03-07 09:46:53 +0100

    fix rm

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ chmod 600 ./deploy_key
 eval "$(ssh-agent)"
 ssh-add ./deploy_key
 git clone git@github.com:pimatic-ci/sqlite3.git repo
-rm -r repo/*
-cp -R ./node_modules/sqlite3 repo
+rm -rf ./repo/*
+cp -R ./node_modules/sqlite3 ./repo
 cd repo
 git rm $(git ls-files --deleted)  
 git config user.email "oliverschneider89+pimatic-ci@gmail.com"
