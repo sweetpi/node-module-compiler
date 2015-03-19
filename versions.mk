57ac3414f2f61e9bb0f49e9c4d0ad290aa969cec
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-03-19 17:58:14 -0400

    Module list update

--- versions.mk
+++ versions.mk
@@ -1,4 +1,4 @@
-pimatic@0.8.62
+pimatic@0.8.63
 pimatic-athome@0.8.2
 pimatic-ble@0.8.0
 pimatic-coc@0.8.3
@@ -18,7 +18,7 @@ pimatic-location@0.1.2
 pimatic-log-reader@0.8.2
 pimatic-mail@0.8.3
 pimatic-max@0.8.10
-pimatic-mobile-frontend@0.8.56
+pimatic-mobile-frontend@0.8.57
 pimatic-mochad@0.8.2
 pimatic-mpd@0.8.9
 pimatic-mysensors@0.8.10
