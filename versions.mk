c06f9abb27c7cb4f98ada268c13091b4fb6f95b3
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-04-16 10:12:20 -0400

    Module list update

--- versions.mk
+++ versions.mk
@@ -5,7 +5,7 @@ pimatic-coc@0.8.5
 pimatic-cron@0.8.5
 pimatic-dht-sensors@0.8.4
 pimatic-dhtxx@0.8.4
-pimatic-ds18b20@0.8.6
+pimatic-ds18b20@0.8.7
 pimatic-edimax@0.0.4
 pimatic-filebrowser@0.8.0
 pimatic-fritz@0.2.2
