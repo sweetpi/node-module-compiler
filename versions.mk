2db4fca53f903e6ff30d2fd9c074d9ad52ae6337
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-04-24 05:56:12 -0400

    Module list update

--- versions.mk
+++ versions.mk
@@ -11,9 +11,9 @@ pimatic-edimax@0.0.5
 pimatic-filebrowser@0.8.0
 pimatic-fritz@0.2.2
 pimatic-gpio@0.8.4
-pimatic-homeduino@0.8.50
+pimatic-homeduino@0.8.51
 pimatic-ifttt@0.1.1
-pimatic-iwy-light-master@0.0.2-1
+pimatic-iwy-light-master@0.0.3
 pimatic-kodi@0.0.7
 pimatic-lcd@0.8.3
 pimatic-links@0.8.5
@@ -26,7 +26,7 @@ pimatic-milight@0.0.4
 pimatic-mobile-frontend@0.8.62
 pimatic-mochad@0.8.2
 pimatic-mpd@0.8.9
-pimatic-mysensors@0.8.13
+pimatic-mysensors@0.8.14
 pimatic-nut@0.1.0
 pimatic-openweather@0.8.6
 pimatic-otgw@0.0.1
@@ -35,7 +35,7 @@ pimatic-piblaster@0.8.1
 pimatic-pilight@0.8.6
 pimatic-ping@0.8.3
 pimatic-play@0.8.0
-pimatic-probe@0.0.3
+pimatic-probe@0.0.4
 pimatic-pushbullet@0.8.4
 pimatic-pushover@0.8.4
 pimatic-sensortag@0.8.3
