415f1cf0b4d76beecb3dc34d0e0b463b1e498d73
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-05-26 14:20:18 -0400

    Module list update

--- versions.mk
+++ versions.mk
@@ -41,7 +41,7 @@ pimatic-pushover@0.8.5
 pimatic-sensortag@0.8.3
 pimatic-shell-execute@0.8.10
 pimatic-sispmctl@0.8.3
-pimatic-solarview@0.0.7
+pimatic-solarview@0.0.8
 pimatic-sonos@0.0.1
 pimatic-sunrise@0.8.1
 pimatic-sysinfo@0.8.13
