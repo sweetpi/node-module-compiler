3f1a653b2e0b19fe0477229925d008c0333a318c
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-05-31 16:36:52 -0400

    Module list update

--- versions.mk
+++ versions.mk
@@ -23,6 +23,7 @@ pimatic-location@0.2.1
 pimatic-log-reader@0.8.5
 pimatic-mail@0.8.3
 pimatic-max@0.8.12
+pimatic-milight@0.0.4
 pimatic-mobile-frontend@0.8.64
 pimatic-mochad@0.8.2
 pimatic-mpd@0.8.9
@@ -30,6 +31,7 @@ pimatic-mysensors@0.8.17
 pimatic-nut@0.1.0
 pimatic-openweather@0.8.9
 pimatic-otgw@0.0.1
+pimatic-ouimeaux@0.1.0
 pimatic-piblaster@0.8.1
 pimatic-ping@0.8.5
 pimatic-play@0.8.0
@@ -50,5 +52,4 @@ pimatic-voice-recognition@0.8.0
 pimatic-volkszaehler@0.2.0
 pimatic-websolarlog@0.0.1
 pimatic-yoctopuce@1.0.7
-pimatic-youless@0.4.1
 pimatic-z-way@0.10.0
