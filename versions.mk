15ae94e5bc64910ea96d4a5c5929d33faede0e51
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-07-04 11:33:36 -0400

    Module list update

--- versions.mk
+++ versions.mk
@@ -54,7 +54,7 @@ pimatic-sunrise@0.8.2
 pimatic-sysinfo@0.8.13
 pimatic-thinkingcleaner@0.8.2
 pimatic-ultrasonic@0.8.4
-pimatic-upnp-root@0.0.6
+pimatic-upnp-root@0.0.7
 pimatic-voice-recognition@0.8.0
 pimatic-volkszaehler@0.2.0
 pimatic-websolarlog@0.0.1
