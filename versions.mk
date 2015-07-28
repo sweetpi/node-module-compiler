38210e369339d4111667706f219208fe64bf53b8
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-07-28 02:11:16 -0400

    Module list update

--- versions.mk
+++ versions.mk
@@ -37,7 +37,7 @@ pimatic-mochad@0.8.2
 pimatic-mpd@0.8.11
 pimatic-mysensors@0.8.20
 pimatic-nut@0.1.0
-pimatic-openweather@0.8.14
+pimatic-openweather@0.8.15
 pimatic-otgw@0.0.1
 pimatic-ouimeaux@0.1.0
 pimatic-piblaster@0.8.1
