97901b869aa7fd766decca8a2769bc2a01cd4f62
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-09-25 12:59:05 -0400

    Module list update

--- versions.mk
+++ versions.mk
@@ -51,7 +51,7 @@ pimatic-prowl@0.0.1
 pimatic-pushbullet@0.8.4
 pimatic-pushover@0.8.5
 pimatic-sensortag@0.8.3
-pimatic-shell-execute@0.8.10
+pimatic-shell-execute@0.8.11
 pimatic-sispmctl@0.8.3
 pimatic-smartmeter@0.0.14
 pimatic-solarview@0.0.9
