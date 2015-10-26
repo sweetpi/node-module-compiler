b4b286ae44e08b841c0dac4b42251aed60ac4845
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-10-26 16:54:00 -0400

    Module list update

--- versions.mk
+++ versions.mk
@@ -18,7 +18,7 @@ pimatic-filter@0.8.2
 pimatic-fritz@0.2.5
 pimatic-fronius-solar@0.0.6
 pimatic-gpio@0.8.9
-pimatic-homeduino@0.8.60
+pimatic-homeduino@0.8.61
 pimatic-homegear@0.0.1
 pimatic-iframe@0.1.2
 pimatic-ifttt@0.1.1
