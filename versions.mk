bd7f70b8b1d4910f32c681911946c6fb9b50964b
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-12-05 07:28:27 -0500

    Module list update

--- versions.mk
+++ versions.mk
@@ -14,7 +14,7 @@ pimatic-edimax@0.3.0
 pimatic-emon@0.2.0
 pimatic-enigma2@0.1.2
 pimatic-filebrowser@0.8.0
-pimatic-filter@0.8.3
+pimatic-filter@0.8.4
 pimatic-fritz@0.3.3
 pimatic-fronius-solar@0.0.7
 pimatic-gpio@0.8.11
