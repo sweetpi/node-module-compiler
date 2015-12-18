f4c3b4c0e69dee5e63e362a384ecf49029c6e146
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-12-18 15:45:54 -0500

    Module list update

--- versions.mk
+++ versions.mk
@@ -48,7 +48,7 @@ pimatic-otgw@0.0.2
 pimatic-ouimeaux@0.1.0
 pimatic-piblaster@0.8.1
 pimatic-ping@0.8.9
-pimatic-pio-remote@0.3.0
+pimatic-pio-remote@0.3.1
 pimatic-play@0.8.0
 pimatic-plex@0.0.1
 pimatic-probe@0.2.2
