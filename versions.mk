cfd4563f5a294258aaaacbed3d1fbd1af000c3e2
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-12-28 15:53:02 -0500

    Module list update

--- versions.mk
+++ versions.mk
@@ -25,7 +25,7 @@ pimatic-homegear@0.0.1
 pimatic-iframe@0.1.2
 pimatic-ifttt@0.1.1
 pimatic-ipcamera@1.1.1
-pimatic-johnny-five@0.8.1
+pimatic-johnny-five@0.8.2
 pimatic-kodi@0.1.5
 pimatic-lcd@0.8.3
 pimatic-led-light@0.3.2
