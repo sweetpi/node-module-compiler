bb9713c5f08b2162a1b9801027acbde2b4ee90fc
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2015-12-31 11:45:30 -0500

    Module list update

--- versions.mk
+++ versions.mk
@@ -29,7 +29,7 @@ pimatic-ipcamera@1.1.1
 pimatic-johnny-five@0.8.2
 pimatic-kodi@0.1.5
 pimatic-lcd@0.8.3
-pimatic-led-light@0.3.2
+pimatic-led-light@0.3.3
 pimatic-links@0.8.5
 pimatic-lirc@0.0.2
 pimatic-lm-sensors@0.1.4
