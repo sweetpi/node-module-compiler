714d3764d1a12cedbe412dccc73e6ff4ab863002
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2016-02-03 07:01:20 -0500

    Module list update

--- versions.mk
+++ versions.mk
@@ -3,11 +3,9 @@ pimatic-angular-material-frontend@0.2.1
 pimatic-athome@0.8.2
 pimatic-bh1750@0.1.1
 pimatic-ble@0.8.0
-pimatic-bluebean@0.8.0
 pimatic-calendar@0.8.0
 pimatic-can@0.1.3
 pimatic-coc@0.8.5
-pimatic-cron@0.8.6
 pimatic-denon-avr@0.8.5
 pimatic-dewpoint@0.8.3
 pimatic-dht-sensors@0.8.4
