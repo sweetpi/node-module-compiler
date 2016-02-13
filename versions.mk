2c00ed7e5cba2c90fab8eb09ee3f05bae3124117
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2016-02-13 07:20:28 -0500

    Module list update

--- versions.mk
+++ versions.mk
@@ -16,7 +16,7 @@ pimatic-easybox@0.1.2
 pimatic-edimax@0.3.3
 pimatic-emon@0.2.0
 pimatic-enigma2@0.1.2
-pimatic-ethboard@0.0.6
+pimatic-ethboard@0.0.7
 pimatic-filebrowser@0.8.0
 pimatic-filter@0.8.4
 pimatic-foscam@0.8.6
