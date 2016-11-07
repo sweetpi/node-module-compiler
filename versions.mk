4fe1ad3416a1e007aa70154a7a7795619ad3df53
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2016-11-07 22:05:59 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -62,7 +62,7 @@ pimatic-mochad-simple@0.6.0
 pimatic-mopidy@0.1.0
 pimatic-mpd@0.8.12
 pimatic-mpower@0.8.3
-pimatic-mqtt@0.9.3
+pimatic-mqtt@0.9.4
 pimatic-mqtt-simple@0.1.1
 pimatic-mysensors@0.8.34
 pimatic-mythermostat@0.0.1
