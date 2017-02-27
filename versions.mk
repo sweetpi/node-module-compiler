2c3ae2765262883e2628ae15e8b3dcb1c44afc8d
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2017-02-27 19:54:58 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -76,7 +76,7 @@ pimatic-mqtt-simple@0.1.1
 pimatic-mysensors@0.8.35
 pimatic-mythermostat@0.0.3
 pimatic-netatmo@0.0.9
-pimatic-node-red@0.0.7
+pimatic-node-red@0.0.8
 pimatic-nuki@0.9.1
 pimatic-nut@0.2.0
 pimatic-openweather@0.9.4
