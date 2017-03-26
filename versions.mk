cfed8a3ceb2480064f1d4ed1e9a25a22641f35b5
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2017-03-26 04:05:38 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -108,6 +108,7 @@ pimatic-shell-execute@0.9.8
 pimatic-sispmctl@0.8.6
 pimatic-slack@1.0.1
 pimatic-smartmeter@0.0.18
+pimatic-smartmeter-gas@0.0.1
 pimatic-smartmeter-old@0.0.17
 pimatic-smartmeter2@0.0.18
 pimatic-sms@1.0.3
