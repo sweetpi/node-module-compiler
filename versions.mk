418c2c2bd8751449fc7d98d4a307ef5a080f836a
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2017-04-26 18:45:38 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -35,7 +35,7 @@ pimatic-filter@0.9.3
 pimatic-foscam@0.8.9
 pimatic-fritz@0.5.5
 pimatic-fronius-solar@0.0.17
-pimatic-google-calendar@0.3.6
+pimatic-google-calendar@0.3.7
 pimatic-gpio@0.8.15
 pimatic-hap@0.10.0
 pimatic-harmonyhub@0.0.6
