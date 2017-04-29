16de80c3c07509cad84bdb0d824e9efc649730e3
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2017-04-29 02:24:39 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -26,7 +26,7 @@ pimatic-dwd@0.1.1
 pimatic-dwd-weather@0.9.0
 pimatic-easybox@0.1.2
 pimatic-echo@0.0.6
-pimatic-edimax@0.3.15
+pimatic-edimax@0.3.16
 pimatic-emon@0.3.0
 pimatic-enigma2@0.1.2
 pimatic-ethboard@0.0.9
