db21060f723e1735ab02d2e8e89a94baba515d4d
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2017-11-25 16:27:37 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -135,7 +135,7 @@ pimatic-sysinfo@0.8.14
 pimatic-telegram@1.1.6
 pimatic-thinkingcleaner@0.8.6
 pimatic-tplink-smartplug@0.0.7
-pimatic-tradfri@0.1.15
+pimatic-tradfri@0.1.16
 pimatic-tv-program@0.0.4
 pimatic-uber@1.0.0
 pimatic-ultrasonic@0.9.1
