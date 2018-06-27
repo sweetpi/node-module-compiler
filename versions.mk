15f5ce452b7ebd9147d536fab3a14dea24a6c631
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2018-06-27 16:05:37 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -54,6 +54,7 @@ pimatic-gpio@0.9.0
 pimatic-hap@0.11.1
 pimatic-harmonyhub@0.0.6
 pimatic-holidays@0.0.3
+pimatic-homeduino@0.9.11
 pimatic-homegear@0.1.1
 pimatic-homegear-ws@0.1.8
 pimatic-hongkongpollution@0.0.1
@@ -78,6 +79,7 @@ pimatic-lm-sensors@0.2.0
 pimatic-location@0.2.5
 pimatic-log-reader@0.9.1
 pimatic-log-to-syslog@1.0.0
+pimatic-luxtronik2@0.0.4
 pimatic-mail@0.8.6
 pimatic-max@0.9.0
 pimatic-maxcul@1.0.7
