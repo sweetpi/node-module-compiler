cfe741efdb5386ae976898745ee94b449e75f7c5
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2018-07-20 02:32:35 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -10,6 +10,7 @@ pimatic-filter@0.9.4
 pimatic-harmonyhub@0.0.6
 pimatic-hue-zll@0.3.1
 pimatic-ifttt@0.1.1
+pimatic-lgtv@0.8.2
 pimatic-log-to-syslog@1.0.0
 pimatic-mail@0.8.6
 pimatic-maxcul@1.0.7
