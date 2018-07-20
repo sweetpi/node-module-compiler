22ff3b7206bd7544b489540c07d242f93cf48c34
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2018-07-20 06:23:36 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -4,11 +4,13 @@ pimatic-bluebean@0.8.0
 pimatic-can@0.1.3
 pimatic-chronotherm@0.9.8-10
 pimatic-dht-sensors@0.9.3
+pimatic-dwd-weather@0.9.5
 pimatic-echo@0.5.3
 pimatic-efergye2@1.0.3
 pimatic-ethboard@0.0.9
 pimatic-filter@0.9.4
 pimatic-harmonyhub@0.0.6
+pimatic-homeduino@0.9.11
 pimatic-hue-zll@0.3.1
 pimatic-ifttt@0.1.1
 pimatic-lgtv@0.8.2
