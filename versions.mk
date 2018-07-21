0f6ca8892c0f8b71c24e49a8d1ee5d5c14f5f33c
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2018-07-21 08:50:36 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -2,6 +2,7 @@ pimatic@0.9.43
 pimatic-angular-material-frontend@0.3.4
 pimatic-anymote@0.1.1
 pimatic-aqara@0.4.4
+pimatic-bh1750@0.2.0
 pimatic-ble@0.9.3
 pimatic-ble-itag@0.0.6
 pimatic-ble-mi-flora@0.0.9
