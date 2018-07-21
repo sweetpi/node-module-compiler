6cc205dd173992f5a1beaedd5144653049a173a7
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2018-07-21 04:41:36 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -57,6 +57,7 @@ pimatic-ping@0.8.14
 pimatic-play@0.8.0
 pimatic-plex@0.0.6
 pimatic-plugin-openhr20@0.16.0
+pimatic-probe@0.3.2
 pimatic-pushbullet@0.9.0
 pimatic-pushover@0.9.4
 pimatic-raspbee@0.0.7
