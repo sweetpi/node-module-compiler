96dc5b40c1e37de4225a93527d0f00561da297c1
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2018-07-22 15:50:37 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -69,6 +69,7 @@ pimatic-max@0.9.0
 pimatic-maxcul@1.0.7
 pimatic-metar-weather@0.9.2
 pimatic-milight@0.0.4
+pimatic-mobile-frontend@0.9.12
 pimatic-mochad@0.8.3
 pimatic-mopidy@0.1.0
 pimatic-mpd@0.8.12
