d506f72d3beebe5f7e7c448358d5e41435c889c3
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2018-07-23 12:14:37 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -31,6 +31,7 @@ pimatic-denon-avr@0.9.10
 pimatic-dewpoint@0.9.2
 pimatic-dht-sensors@0.9.3
 pimatic-diy-controls@0.0.1
+pimatic-dlnacasts@0.1.0
 pimatic-dooya@1.0.0
 pimatic-ds18b20@0.8.9
 pimatic-dwd@0.9.1
