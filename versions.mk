08adfb2249fe8faa7e6e3825fe873d0cc4efec9e
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2018-07-23 12:32:37 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -27,6 +27,7 @@ pimatic-cron@0.8.8
 pimatic-css-inject@0.1.1
 pimatic-customcss@0.1.0
 pimatic-dash-button@0.2.1
+pimatic-datetime@0.3.1
 pimatic-denon-avr@0.9.10
 pimatic-dewpoint@0.9.2
 pimatic-dht-sensors@0.9.3
