f503c81071540753c8ad2bf707686d7cf7f05717
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2018-10-13 16:29:13 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -1,8 +1,8 @@
 pimatic@0.9.43
 pimatic-alarm@0.2.0
 pimatic-alert@0.3.7
+pimatic-amazing-dash-button@0.9.14
 pimatic-angular-material-frontend@0.3.4
-pimatic-anymote@0.1.1
 pimatic-aqara@0.4.4
 pimatic-athome@0.9.1
 pimatic-backup@0.2.0
@@ -11,8 +11,8 @@ pimatic-ble-itag@0.0.6
 pimatic-ble-mi-flora@0.0.9
 pimatic-bluebean@0.8.0
 pimatic-bme280@0.1.3
+pimatic-bmp180@2.0.1
 pimatic-bmp280@0.1.0
-pimatic-buienradar@0.1.4
 pimatic-calendar@0.8.0
 pimatic-can@0.1.3
 pimatic-charts@0.0.4
@@ -21,8 +21,8 @@ pimatic-climasens@0.1.3
 pimatic-coc@0.9.0
 pimatic-cron@0.8.8
 pimatic-css-inject@0.1.1
+pimatic-customcss@0.1.0
 pimatic-dash-button@0.2.1
-pimatic-datetime@0.3.1
 pimatic-denon-avr@0.9.11
 pimatic-dewpoint@0.9.2
 pimatic-dht-sensors@0.9.3
@@ -31,8 +31,8 @@ pimatic-dooya@1.0.0
 pimatic-ds18b20@0.8.9
 pimatic-dwd@0.9.2
 pimatic-dwd-weather@0.9.5
+pimatic-easybox@0.1.2
 pimatic-echo@0.5.3
-pimatic-edimax@0.3.19
 pimatic-efergye2@1.0.3
 pimatic-emon@0.3.0
 pimatic-enigma2@0.1.2
@@ -41,8 +41,8 @@ pimatic-filter@0.9.4
 pimatic-foscam@0.8.9
 pimatic-fritz@0.6.6
 pimatic-fronius-solar@0.0.17
+pimatic-google-calendar@0.4.1
 pimatic-gpio@0.9.0
-pimatic-hap@0.11.1
 pimatic-harmonyhub@0.0.6
 pimatic-hk-avr@0.1.0
 pimatic-holidays@0.0.3
@@ -51,8 +51,8 @@ pimatic-homegear-ws@0.1.8
 pimatic-hongkongpollution@0.0.1
 pimatic-hue@0.1.0
 pimatic-hue-zll@0.3.1
+pimatic-i2c-bme280@0.9.1
 pimatic-iframe@0.3.1
-pimatic-ifttt@0.1.1
 pimatic-intergasincomfort@0.3.0
 pimatic-ipcamera@1.1.1
 pimatic-jeelabs@0.8.7
@@ -61,8 +61,8 @@ pimatic-kodi@0.1.9
 pimatic-lcd@0.8.3
 pimatic-led-light@0.9.5
 pimatic-lgtv@0.8.2
+pimatic-links@0.8.6
 pimatic-lirc@0.1.1
-pimatic-livolo@1.1.1
 pimatic-lm-sensors@0.2.0
 pimatic-location@0.2.5
 pimatic-log-reader@0.9.2
@@ -71,8 +71,8 @@ pimatic-mail@0.8.6
 pimatic-max@0.9.0
 pimatic-maxcul@1.0.7
 pimatic-metar-weather@0.9.2
+pimatic-milight@0.0.4
 pimatic-milight-reloaded@0.9.17
-pimatic-mjpg@0.0.1
 pimatic-mobile-frontend@0.9.12
 pimatic-mochad@0.8.3
 pimatic-mochad-simple@0.6.0
@@ -81,8 +81,8 @@ pimatic-mpower@0.8.3
 pimatic-mqtt@0.9.10
 pimatic-mqtt-simple@0.1.1
 pimatic-mysensors@0.9.2
+pimatic-mythermostat@0.0.3
 pimatic-nanoleaf@0.0.4
-pimatic-nefit@0.1.19
 pimatic-netatmo@0.0.9
 pimatic-netcheck@1.1.0
 pimatic-nikoniko@0.8.2
@@ -91,8 +91,8 @@ pimatic-nut@0.2.0
 pimatic-obi-power-outlet@0.2.0
 pimatic-onkyo@0.2.0
 pimatic-onkyo-avr@0.2.0
+pimatic-openweather@0.9.4
 pimatic-orvibo@0.0.3
-pimatic-otgw@0.0.2
 pimatic-ouimeaux@0.1.0
 pimatic-owjs@0.1.7
 pimatic-owntracks@1.0.2
@@ -101,8 +101,8 @@ pimatic-piblaster@0.9.0
 pimatic-ping@0.8.14
 pimatic-pio-remote@0.5.3
 pimatic-play@0.8.0
+pimatic-plex@0.0.6
 pimatic-plugin-openhr20@0.16.0
-pimatic-probe@0.3.2
 pimatic-prowl@0.0.2
 pimatic-ps4waker@0.1.4
 pimatic-pushbullet@0.9.0
@@ -111,8 +111,8 @@ pimatic-quote-of-the-day@1.0.7
 pimatic-radiohead@1.3.1
 pimatic-raspbee@0.0.7
 pimatic-rflink@0.7.2
+pimatic-rfxcom@1.0.4
 pimatic-rpi433@0.1.0
-pimatic-samsung-tv@0.9.0
 pimatic-samsung-tv-2016@0.4.0
 pimatic-sensibo@0.9.0-a.4
 pimatic-sensortag@0.8.3
@@ -121,8 +121,8 @@ pimatic-serial-ivisions@0.0.2
 pimatic-shell-execute@0.9.11
 pimatic-sispmctl@0.8.6
 pimatic-slack@1.0.1
+pimatic-smartmeter@0.0.18
 pimatic-smartmeter-gas@0.0.1
-pimatic-smartmeter-old@0.0.17
 pimatic-smartmeter2@0.0.18
 pimatic-sms@1.0.3
 pimatic-snmp@0.4.0
@@ -131,8 +131,8 @@ pimatic-sonos@0.1.1
 pimatic-sunrise@0.9.2
 pimatic-sysinfo@0.8.14
 pimatic-tankerkoenig@0.0.4
+pimatic-telegram@1.1.11
 pimatic-thinkingcleaner@0.8.6
-pimatic-tplink-smartplug@0.0.7
 pimatic-tradfri@0.1.19
 pimatic-tts@0.0.5
 pimatic-tv-program@0.1.3
@@ -141,8 +141,8 @@ pimatic-unieq-box@0.9.0
 pimatic-unipi-evok@0.3.1
 pimatic-upnp-root@0.9.0
 pimatic-voice-recognition@0.8.0
+pimatic-volkszaehler@0.3.1
 pimatic-vr-harmonyhub@0.0.6
-pimatic-vsphere@0.1.1
 pimatic-vvo@0.0.7
 pimatic-wago@1.0.0
 pimatic-wakeonlan@0.9.4
@@ -151,8 +151,8 @@ pimatic-websolarlog@0.0.5
 pimatic-wemo@0.0.3
 pimatic-wifiswitch@0.0.3
 pimatic-wink@1.1.0
+pimatic-wmi@0.1.7
 pimatic-wunderground@0.2.4
-pimatic-xbox@0.1.1
 pimatic-xmpp@0.0.11
 pimatic-yamaha-avr@0.9.4
 pimatic-yoctopuce@1.0.7
