1c91e85a378464db4b3f4ef634b64bcfe76b7afe
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2018-12-27 19:41:37 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -80,6 +80,7 @@ pimatic-lm-sensors@0.2.0
 pimatic-location@0.2.5
 pimatic-log-reader@0.9.2
 pimatic-log-to-syslog@1.0.0
+pimatic-luftdaten@0.1.0
 pimatic-luxtronik2@0.0.4
 pimatic-mail@0.8.6
 pimatic-max@0.9.0
