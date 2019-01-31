bd0b258f09bf257343945e9e2357762d6171b64d
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2019-02-01 00:56:12 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -64,7 +64,6 @@ pimatic-hue@0.1.0
 pimatic-hue-zll@0.3.1
 pimatic-i2c-bme280@0.9.1
 pimatic-iframe@0.3.1
-pimatic-ifttt@0.1.1
 pimatic-intergasincomfort@0.3.0
 pimatic-ipcamera@1.1.1
 pimatic-jeelabs@0.8.7
