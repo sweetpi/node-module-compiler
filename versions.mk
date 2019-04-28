f2140af43446990768e905758c074d6a302a020d
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2019-04-29 01:36:37 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -56,7 +56,7 @@ pimatic-hap@0.11.1
 pimatic-harmonyhub@0.0.6
 pimatic-hk-avr@0.1.0
 pimatic-holidays@0.0.3
-pimatic-homeduino@0.9.14
+pimatic-homeduino@0.9.15
 pimatic-homegear@0.1.1
 pimatic-homegear-ws@0.1.8
 pimatic-hongkongpollution@0.0.1
