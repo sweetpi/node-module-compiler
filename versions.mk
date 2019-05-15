83d8488a6e70308a1a574add36af12870066110e
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2019-05-15 09:59:37 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -119,6 +119,7 @@ pimatic-owjs@0.1.7
 pimatic-owntracks@1.0.2
 pimatic-pcf8574@1.0.6
 pimatic-phone@0.9.7
+pimatic-piblaster@0.9.0
 pimatic-ping@0.8.14
 pimatic-pio-remote@0.5.3
 pimatic-play@0.8.0
@@ -180,6 +181,7 @@ pimatic-wakeonlan@0.9.4
 pimatic-watchdog-unix@0.0.1
 pimatic-water-level@0.0.5
 pimatic-websolarlog@0.0.5
+pimatic-wemo@0.0.3
 pimatic-wifiswitch@0.0.3
 pimatic-wink@1.1.0
 pimatic-wmi@0.1.7
