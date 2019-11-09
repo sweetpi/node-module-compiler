bd24c8df486436e76aa77f2a10674bd1687e94f4
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2019-11-09 12:23:37 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -143,6 +143,7 @@ pimatic-samsung-tv@0.9.0
 pimatic-samsung-tv-2016@0.4.0
 pimatic-sensibo@0.9.0-a.4
 pimatic-sensortag@0.8.3
+pimatic-serial@0.0.3
 pimatic-serial-benqbeamer@0.0.2
 pimatic-serial-ivisions@0.0.2
 pimatic-shell-execute@0.9.11
