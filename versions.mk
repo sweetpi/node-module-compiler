504aa0ef13199cd064bb5eda8a884b061adf2eab
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2020-01-18 11:18:40 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -145,7 +145,7 @@ pimatic-rpi433@0.1.0
 pimatic-rtl433@1.0.0
 pimatic-samsung-tv@0.9.0
 pimatic-samsung-tv-2016@0.4.0
-pimatic-save@0.1.1
+pimatic-save@0.1.2
 pimatic-sensibo@0.9.0-a.4
 pimatic-sensortag@0.8.3
 pimatic-serial@0.0.3
