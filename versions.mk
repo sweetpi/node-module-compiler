0e19af1d588476f3f14c2f1c8a020ba0ee99d04b
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2020-02-13 14:54:39 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -120,7 +120,6 @@ pimatic-onkyo-avr@0.2.0
 pimatic-openweather@0.9.4
 pimatic-orvibo@0.0.3
 pimatic-otgw@0.0.2
-pimatic-ouimeaux@0.1.0
 pimatic-owjs@0.1.7
 pimatic-owntracks@1.0.2
 pimatic-pcf8574@1.0.6
