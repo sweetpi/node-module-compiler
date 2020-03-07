a94c26a51df5c3f7a3620f3f45edd15733adca54
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2020-03-07 02:09:40 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -30,7 +30,6 @@ pimatic-css-inject@0.1.2
 pimatic-customcss@0.1.0
 pimatic-dash-button@0.2.1
 pimatic-datetime@0.3.1
-pimatic-deebot@0.0.23
 pimatic-denon-avr@0.9.11
 pimatic-dewpoint@0.9.2
 pimatic-dht-sensors@0.9.3
@@ -60,7 +59,6 @@ pimatic-harmonyhub@0.0.6
 pimatic-hass@0.1.5
 pimatic-hk-avr@0.1.0
 pimatic-holidays@0.0.3
-pimatic-homeduino@0.9.18
 pimatic-homegear@0.1.1
 pimatic-homegear-ws@0.1.8
 pimatic-hongkongpollution@0.0.1
@@ -109,6 +107,7 @@ pimatic-mysensors@0.9.3
 pimatic-mythermostat@0.0.3
 pimatic-nanoleaf@0.0.4
 pimatic-nefit@0.1.19
+pimatic-netatmo@0.0.9
 pimatic-netcheck@1.1.0
 pimatic-nikoniko@0.8.2
 pimatic-node-red@1.0.7
