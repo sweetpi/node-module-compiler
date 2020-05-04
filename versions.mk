23439a44596b33746eacfd874e5e7f65a929849c
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2020-05-04 21:18:41 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -22,7 +22,7 @@ pimatic-calendar2@0.9.9
 pimatic-can@0.1.3
 pimatic-charts@0.0.4
 pimatic-check-alive@0.1.0
-pimatic-chromecast@1.2.0
+pimatic-chromecast@1.2.1
 pimatic-chronotherm@0.9.8-10
 pimatic-chronothermmanu@0.9.8-11
 pimatic-climasens@0.1.5
