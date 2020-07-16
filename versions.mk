95bddd21393164b6d9c15985664bfde46913c2c2
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2020-07-16 18:45:41 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -12,7 +12,7 @@ pimatic-bh1750@0.2.0
 pimatic-ble@0.9.3
 pimatic-ble-itag@0.0.6
 pimatic-ble-mi-flora@0.0.9
-pimatic-ble2@0.9.6
+pimatic-ble2@0.9.7
 pimatic-ble2-mi-flora@0.0.9
 pimatic-bluebean@0.8.0
 pimatic-bme280@0.1.3
