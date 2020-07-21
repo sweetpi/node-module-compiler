f1a55a69ff19b8477cced70a99d2aefd27f31093
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2020-07-21 10:18:40 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -151,7 +151,7 @@ pimatic-pushover@0.9.4
 pimatic-pushsafer@1.0.5
 pimatic-quote-of-the-day@1.0.7
 pimatic-radiohead@2.0.0
-pimatic-raspbee@0.1.6
+pimatic-raspbee@0.1.7
 pimatic-rfl868@0.7.3
 pimatic-rflink@0.7.2
 pimatic-rfxcom@1.1.0
