bc0d69d57d7927ffce3c93b4cc8793e794f518d1
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2020-08-07 22:19:07 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -65,7 +65,6 @@ pimatic-google-calendar@0.4.1
 pimatic-gpio@0.9.1
 pimatic-hap@0.13.0
 pimatic-harmonyhub@0.0.6
-pimatic-hass@0.1.5
 pimatic-hk-avr@0.1.0
 pimatic-holidays@0.0.3
 pimatic-home-connect@0.2.17
