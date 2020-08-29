68ae98053f20f09440b10eca7f192068c9cff869
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2020-08-29 20:27:42 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -102,7 +102,7 @@ pimatic-luxtronik2@0.0.4
 pimatic-mail@0.8.6
 pimatic-max@0.9.0
 pimatic-maxcul@1.1.1
-pimatic-meross@0.2.1
+pimatic-meross@0.2.2
 pimatic-mesh@0.2.1
 pimatic-metar-weather@0.9.2
 pimatic-milight@0.0.4
