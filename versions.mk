58fbfb00a12d466b465f6b210f668be3e2815ee0
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2020-10-04 21:27:42 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -194,7 +194,7 @@ pimatic-tools@0.0.5
 pimatic-tplink-smartplug@0.0.8
 pimatic-tradfri@0.1.22
 pimatic-tts@0.0.5
-pimatic-tuya@0.0.14
+pimatic-tuya@0.0.15
 pimatic-tv-program@0.1.3
 pimatic-uber@1.0.0
 pimatic-ultrasonic@0.9.1
