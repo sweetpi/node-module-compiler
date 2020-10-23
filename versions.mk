c5ad014ad7a59103ca03e5259cb2e92b645f07e2
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2020-10-23 18:50:38 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -176,6 +176,7 @@ pimatic-smartmeter-obis@0.1.17
 pimatic-smartmeter-old@0.0.17
 pimatic-smartmeter-stats@0.1.24
 pimatic-smartmeter2@0.0.18
+pimatic-smartmeter3@0.0.6
 pimatic-sms@1.0.3
 pimatic-snmp@0.4.2
 pimatic-solarview@0.8.6
