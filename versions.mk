1e1786a522d4480dfbe5bc825863e82c6e265568
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2021-01-11 13:00:41 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -208,7 +208,6 @@ pimatic-upnp-root@0.9.0
 pimatic-voice-recognition@0.8.0
 pimatic-volkszaehler@0.3.1
 pimatic-vr-harmonyhub@0.0.6
-pimatic-vsphere@0.1.1
 pimatic-vvo@0.0.7
 pimatic-wago@1.0.0
 pimatic-wakeonlan@0.9.7
