2d31198352eaa453bbc8a11d991686bfcb1125cc
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2021-01-12 16:14:39 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -202,6 +202,7 @@ pimatic-tuya@0.0.15
 pimatic-tv-program@0.1.3
 pimatic-uber@1.0.0
 pimatic-ultrasonic@0.9.1
+pimatic-unieq-box@0.9.0
 pimatic-unipi-evok@0.3.3
 pimatic-upnp-root@0.9.0
 pimatic-voice-recognition@0.8.0
