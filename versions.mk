d1f3521fc2ef6e9f5306d730cc79d6fa6599ee38
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2021-01-15 17:05:40 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -188,6 +188,7 @@ pimatic-sounds@0.2.16
 pimatic-sounds-basic@0.1.24
 pimatic-spritpreise@1.0.0
 pimatic-stats@0.1.14
+pimatic-sunrise@0.9.3
 pimatic-sysinfo@0.9.6
 pimatic-tado-reloaded@0.2.3
 pimatic-tankerkoenig@0.0.4
