b2bac7e9e8c20643c8a7a0e66be897c1e8d678ff
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2021-06-15 21:23:39 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -100,6 +100,7 @@ pimatic-lirc@0.1.1
 pimatic-livolo@1.1.2
 pimatic-lm-sensors@0.2.0
 pimatic-location@0.2.5
+pimatic-log-reader@0.9.3
 pimatic-luftdaten@0.4.8
 pimatic-luxtronik2@0.0.4
 pimatic-mail@0.8.6
