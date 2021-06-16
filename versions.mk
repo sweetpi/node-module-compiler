e923907c0457c5b6a7244e6a8b8863353dd6de2d
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2021-06-16 12:32:38 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -69,7 +69,6 @@ pimatic-hap@0.13.0
 pimatic-harmonyhub@0.0.6
 pimatic-hass@0.3.10
 pimatic-hk-avr@0.1.0
-pimatic-holidays@0.0.3
 pimatic-home-connect@0.2.17
 pimatic-homeduino@0.9.18
 pimatic-homegear@0.1.1
@@ -138,6 +137,7 @@ pimatic-openweather@0.9.4
 pimatic-orvibo@0.0.3
 pimatic-otgw@0.0.2
 pimatic-ouimeaux@0.1.0
+pimatic-owjs@0.1.7
 pimatic-owntracks@1.0.2
 pimatic-phone@0.9.7
 pimatic-piblaster@0.9.0
