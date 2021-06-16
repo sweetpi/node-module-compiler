e064772e64bef5b388706336f3b644ef9c3f0635
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2021-06-17 01:27:40 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -177,7 +177,6 @@ pimatic-sma@0.0.14
 pimatic-smartmeter@0.0.18
 pimatic-smartmeter-gas@0.0.1
 pimatic-smartmeter-obis@0.1.17
-pimatic-smartmeter-old@0.0.17
 pimatic-smartmeter-stats@0.1.24
 pimatic-smartmeter2@0.0.18
 pimatic-smartmeter3@0.0.6
