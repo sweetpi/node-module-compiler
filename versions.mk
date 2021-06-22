e25cda692c8ecdf91c341c65c5baccad85ec84b4
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2021-06-22 03:32:38 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -167,6 +167,7 @@ pimatic-save@0.1.24
 pimatic-sensibo@0.9.0-a.4
 pimatic-sensortag@0.8.3
 pimatic-serial@0.0.3
+pimatic-serial-benqbeamer@0.0.2
 pimatic-serial-ivisions@0.0.2
 pimatic-shell-execute@0.9.11
 pimatic-sispmctl@0.8.6
@@ -230,4 +231,5 @@ pimatic-yamaha-avr@0.9.4
 pimatic-yoctopuce@1.0.7
 pimatic-youless@0.5.0
 pimatic-z-way@1.4.3
+pimatic-ziggo-mediabox@0.1.0
 pimatic-zwave-usb@0.0.11
