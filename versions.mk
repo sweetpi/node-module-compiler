4c208dd76ad52e12c5c446f07578db0d40e458ea
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2021-06-22 20:45:41 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -149,6 +149,7 @@ pimatic-plex@0.0.6
 pimatic-plugin-openhr20@0.17.0
 pimatic-probe@0.3.4
 pimatic-prowl@0.0.2
+pimatic-ps4waker@0.1.4
 pimatic-pushbullet@0.9.0
 pimatic-pushover@0.9.4
 pimatic-pushsafer@1.0.5
