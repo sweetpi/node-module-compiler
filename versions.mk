fec91ef78dafb5cbfbdb537e5c405b31651cb3e2
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2022-02-21 17:18:38 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -214,7 +214,7 @@ pimatic-vsphere@0.1.1
 pimatic-vvo@0.0.7
 pimatic-wago@1.0.0
 pimatic-wakeonlan@0.9.7
-pimatic-wakeuplight@0.0.6
+pimatic-wakeuplight@0.0.7
 pimatic-watchdog-unix@0.0.1
 pimatic-water-level@0.0.5
 pimatic-wavtrigger@0.0.6
