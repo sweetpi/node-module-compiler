041477e8e476cf187ea8087bdcff226e2ce7ebb2
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2022-03-15 20:27:38 +0100

    Module list update

--- versions.mk
+++ versions.mk
@@ -130,7 +130,7 @@ pimatic-node-red@1.0.7
 pimatic-nuki@0.9.1
 pimatic-nuki-reloaded@0.9.28
 pimatic-nut@0.2.0
-pimatic-nzbget@0.0.5
+pimatic-nzbget@0.0.6
 pimatic-obi-power-outlet@0.2.0
 pimatic-onkyo-avr@0.2.0
 pimatic-openweather@0.9.4
