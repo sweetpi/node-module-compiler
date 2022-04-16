4d7bf026c1d8b9cd97f3fb49e0543dd682b64114
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2022-04-16 14:18:38 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -56,6 +56,7 @@ pimatic-enigma2@0.1.2
 pimatic-esp-climate@0.0.1
 pimatic-ethboard@0.0.9
 pimatic-filebrowser@0.9.2
+pimatic-filter@0.9.5
 pimatic-floorplan@0.2.13
 pimatic-foscam@0.8.9
 pimatic-fritz@0.6.7
