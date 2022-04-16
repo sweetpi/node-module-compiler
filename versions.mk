09211a8acd6c531e46e3919d475da5f289bc1464
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2022-04-16 11:09:38 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -147,7 +147,6 @@ pimatic-play@0.8.0
 pimatic-plex@0.0.6
 pimatic-plugin-openhr20@0.17.0
 pimatic-probe@0.3.4
-pimatic-prowl@0.0.2
 pimatic-pushbullet@0.9.0
 pimatic-pushover@0.9.4
 pimatic-pushsafer@1.0.5
@@ -228,6 +227,7 @@ pimatic-wunderground@0.2.4
 pimatic-xbox@0.1.1
 pimatic-xmpp@0.0.11
 pimatic-yamaha-avr@0.9.4
+pimatic-yoctopuce@1.0.7
 pimatic-youless@0.5.0
 pimatic-z-way@1.4.3
 pimatic-ziggo-mediabox@0.1.0
