ffac0b1653d150d102a7880dc2497a1fd9eddc80
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2022-04-16 08:32:36 +0200

    Module list update

--- versions.mk
+++ versions.mk
@@ -48,6 +48,7 @@ pimatic-dwd@0.9.2
 pimatic-dwd-weather@0.9.5
 pimatic-dyson@0.1.3
 pimatic-easybox@0.1.2
+pimatic-echo@0.6.0
 pimatic-edimax@0.3.19
 pimatic-efergye2@1.0.4
 pimatic-emon@0.3.0
@@ -100,6 +101,7 @@ pimatic-lm-sensors@0.2.0
 pimatic-log-reader@0.9.3
 pimatic-luftdaten@0.4.8
 pimatic-luxtronik2@0.0.4
+pimatic-mail@0.8.6
 pimatic-max@0.9.0
 pimatic-maxcul@1.1.1
 pimatic-meross@0.2.8
@@ -112,6 +114,7 @@ pimatic-mobile-frontend@0.9.16
 pimatic-mochad@0.9.53
 pimatic-mochad-simple@0.6.0
 pimatic-mopidy@0.1.0
+pimatic-mpd@0.8.12
 pimatic-mpower@0.8.3
 pimatic-mqtt@0.9.13
 pimatic-mqtt-ext@0.9.8
@@ -136,6 +139,7 @@ pimatic-otgw@0.0.2
 pimatic-ouimeaux@0.1.0
 pimatic-owjs@0.1.7
 pimatic-owntracks@1.0.2
+pimatic-phone@0.9.7
 pimatic-piblaster@0.9.0
 pimatic-ping@0.8.14
 pimatic-pio-remote@0.5.3
@@ -170,7 +174,6 @@ pimatic-slack@1.0.1
 pimatic-slide@0.9.6
 pimatic-sma@0.0.15
 pimatic-smartmeter@0.0.18
-pimatic-smartmeter-gas@0.0.1
 pimatic-smartmeter-obis@0.1.17
 pimatic-smartmeter-old@0.0.17
 pimatic-smartmeter-stats@0.1.24
@@ -189,8 +192,8 @@ pimatic-sunrise@0.9.3
 pimatic-switchbot@0.0.3
 pimatic-sysinfo@0.9.6
 pimatic-tado-reloaded@0.2.5
+pimatic-tankerkoenig@0.0.4
 pimatic-telegram@1.1.14
-pimatic-thinkingcleaner@0.8.6
 pimatic-tools@0.0.5
 pimatic-tplink-smartplug@0.0.8
 pimatic-tradfri@0.1.22
@@ -213,7 +216,6 @@ pimatic-wakeuplight@0.0.11
 pimatic-watchdog-unix@0.0.1
 pimatic-water-level@0.0.5
 pimatic-wavtrigger@0.0.6
-pimatic-websolarlog@0.0.5
 pimatic-wemo@0.0.3
 pimatic-wifiswitch@0.0.3
 pimatic-wink@1.1.0
