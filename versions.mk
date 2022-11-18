56e6dfe348636580351d871392c7cb5e711eaba8
pimatic-ci <oliverschneider89+pimatic-ci@gmail.com>
2022-11-18 02:01:48 +0000

    Module list update

--- versions.mk
+++ versions.mk
@@ -184,6 +184,7 @@ pimatic-sms@1.0.3
 pimatic-snmp@0.4.2
 pimatic-solarview@0.8.6
 pimatic-solvisremote@0.0.5
+pimatic-sonos@0.1.1
 pimatic-sounds@0.2.16
 pimatic-sounds-basic@0.1.24
 pimatic-spotify@0.0.3
